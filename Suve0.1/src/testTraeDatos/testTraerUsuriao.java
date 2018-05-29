a0636e0cb4fc33126f6e42b41da504ea3c11e886
reypala <lucastisera@gmail.com>
2018-05-29 14:48:38 -0300

    ESTE ES EL COMMIT DE ENTREGA

--- Suve0.1/src/testTraeDatos/testTraerUsuriao.java
+++ Suve0.1/src/testTraeDatos/testTraerUsuriao.java
@@ -3,7 +3,7 @@ package testTraeDatos;
 import dao.UsuarioDao;
 
 public class testTraerUsuriao {
-
+//ESTA ES LA ENTREGA
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		UsuarioDao udao = new UsuarioDao();
